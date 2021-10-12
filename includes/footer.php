5e5bd44f1f79d3f54ec20b4024074908e7b377c2
BernardKwesi <bernarddormon31@gmail.com>
2021-10-12 17:09:21 +0000

    User Registration Form for Conference

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
-<footer>
+<footer style="text-align:center ;">
     <div>
         <p>COPYRIGHT @ <?php echo date('y') ?></p>
     </div>
@@ -8,7 +8,16 @@
 </div>
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> -->
+<link rel="stylesheet" href="//code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css">
+<!-- <link rel="stylesheet" href="/resources/demos/style.css"> -->
+<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+<script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>
+<script>
+    $(function() {
+        $("#dob").datepicker();
+    });
+</script>
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -4,6 +4,52 @@ $title = "index";
 ?>
 
 
-<h1>Hello World!!</h1>
+<h1 class="mt-2 mb-3" style="text-align: center;">Registration For Conference.</h1>
+<form>
+
+    <div class="mb-3">
+        <label for="firstName" class="form-label">First Name</label>
+        <input type="text" class="form-control" id="firstName">
+
+    </div>
+    <div class="mb-3">
+        <label for="lastName" class="form-label">Last Name</label>
+        <input type="text" class="form-control" id="lastName">
+
+    </div>
+
+    <div class="mb-3">
+        <label for="dob" class="form-label">Date of Birth</label>
+        <input type="text" class="form-control" id="dob" placeholder="MM/DD/YYYY">
+
+    </div>
+
+    <div class="mb-3">
+        <label for="email" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
+        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    </div>
+
+    <div class="mb-3">
+        <label for="contact" class="form-label">Contact Number</label>
+        <input type="number" class="form-control" id="contact">
+        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+
+    </div>
+    <div class="mb-3">
+        <label for="speciality" class="form-label">Speciality</label>
+        <select class="form-select" aria-label="Default select example">
+
+            <option selected value="1">Software Developer</option>
+            <option value="2">Database Administrator</option>
+            <option value="3">Web Administrator</option>
+            <option value="">Other</option>
+        </select>
+    </div>
+    <div class="d-grid ">
+        <button type="submit" class="btn btn-primary  mb-5 btn-block">Submit</button>
+
+    </div>
+</form>
 
 <?php require_once "includes/footer.php"; ?>
